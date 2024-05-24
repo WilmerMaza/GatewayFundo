5700b83d4a6661fef6839ec74a460463095f6ccf
WilmerMaza <wilmermaza10@gmail.com>
2024-05-24 01:58:40 -0500

    limpiar

--- dist/routes/statusRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/routes/userRoutes.ts
-const express_1 = require("express");
-const statusController_1 = require("../controllers/statusController");
-const router = (0, express_1.Router)();
-router.get('/', statusController_1.getStatus);
-exports.default = router;
-//# sourceMappingURL=statusRoutes.js.map
--- dist/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/routes/userRoutes.ts
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const router = (0, express_1.Router)();
-router.get('/', userController_1.getUsers);
-exports.default = router;
-//# sourceMappingURL=userRoutes.js.map
